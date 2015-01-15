175f0d694dd4d4af06d718ddef79cfb1bbf73929
ice6294 <lg.leon@alumnos.urjc.es>
2015-01-15 17:24:51 +0100

    EnReunion mejorado

--- GrapeVine/src/grapevine/Observador.java
+++ GrapeVine/src/grapevine/Observador.java
@@ -7,12 +7,13 @@ import java.util.ArrayList;
 public class Observador {
     
     ArrayList <Observador> restoParticipantes;
+    Reunion reunion;
 
     // CONSTRUCTOR
-    public Observador(ArrayList<Observador> restoParticipantes) {
+    public Observador(ArrayList<Observador> restoParticipantes, Reunion reunion) {
         this.restoParticipantes = restoParticipantes;
+        this.reunion=reunion;
     }
-
 
     // GETTERS & SETTERS
 
@@ -24,23 +25,17 @@ public class Observador {
         this.restoParticipantes = restoParticipantes;
     }
     
-    
     //FUNCIONES DE PASO DE MENSAJE
     
     public void actualizaRestoParticipantes(Mensaje m){
         
         for (int i=0; i<restoParticipantes.size();i++){
-            
             restoParticipantes.get(i).actualizarme(m);
-            
         }
         
     }
     
     public void actualizarme(Mensaje m){
-        
-        
-        
+        this.reunion.escribir(m);
     }
-
 }
